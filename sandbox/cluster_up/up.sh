5fcc43f746e443574fe0329abe490b348d357dcd
Charles Moulliard <cmoulliard@redhat.com>
2018-09-14 10:29:57 +0200

    Move oc to /usr/bin

--- sandbox/cluster_up/up.sh
+++ sandbox/cluster_up/up.sh
@@ -4,8 +4,7 @@ echo "==============================="
 echo "Download oc client, untar it"
 echo "==============================="
 wget -O- https://github.com/openshift/origin/releases/download/v3.10.0/openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit.tar.gz | tar vxz
-cd openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit/
-export PATH=$(pwd):$PATH
+sudo cp openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit/oc /usr/bin
 
 echo "==============================="
 echo "Configure docker to be insecure"
@@ -17,7 +16,9 @@ echo "==============================="
 echo "Bootstrap oc"
 echo "==============================="
 PUBLIC_IP=$(ifconfig enp0s3 | sed -En 's/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-oc cluster up --public-hostname=${PUBLIC_IP}
+oc cluster up \
+  --base-dir="/var/lib/origin/openshift.local.clusterup" \
+  --public-hostname=${PUBLIC_IP}
 
 echo "==============================="
 echo "Grant cluster-admin role to admin's user"
