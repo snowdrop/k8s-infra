bc6e445dd71365d55b8d3b19a3819eac2938bb1b
Charles Moulliard <cmoulliard@redhat.com>
2018-09-14 19:14:40 +0200

    Refactor list and add missing images needed by ASB

--- sandbox/cluster_up/docker_save_images.sh
+++ sandbox/cluster_up/docker_save_images.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 
-images="openshift/origin-node:v3.10 openshift/origin-docker-builder:v3.10 openshift/origin-haproxy-router:v3.10 openshift/origin-deployer:v3.10 openshift/origin-control-plane:v3.10 openshift/origin-hypershift:v3.10 openshift/origin-hyperkube:v3.10 openshift/origin-pod:v3.10 openshift/origin-web-console:v3.10 openshift/origin-docker-registry:v3.10 openshift/origin-cli:v3.10"
-docker save $images > /media/sf_share/origin-v3.10.tar
+images="quay.io/coreos/etcd:v3.3\
+ openshift/origin-service-catalog:v3.10\
+ quay.io/coreos/etcd:v3.3\
+ docker.io/automationbroker/automation-broker-apb:latest\
+ openshift/origin-service-catalog:v3.10\
+ openshift/origin-node:v3.10\
+ openshift/origin-docker-builder:v3.10\
+ openshift/origin-haproxy-router:v3.10\
+ openshift/origin-deployer:v3.10\
+ openshift/origin-control-plane:v3.10\
+ openshift/origin-hypershift:v3.10\
+ openshift/origin-hyperkube:v3.10\
+ openshift/origin-pod:v3.10\
+ openshift/origin-web-console:v3.10\
+ openshift/origin-docker-registry:v3.10\
+ openshift/origin-cli:v3.10"
+docker save $images > /media/sf_shared/origin-v3.10.tar
