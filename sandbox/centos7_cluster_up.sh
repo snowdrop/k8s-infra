d9c0e7aa3da38084fb30b8880a8428d1d73b55fc
Charles Moulliard <cmoulliard@redhat.com>
2018-09-14 07:21:00 +0200

    Set var

--- sandbox/centos7_cluster_up.sh
+++ sandbox/centos7_cluster_up.sh
@@ -4,10 +4,11 @@
 # Command to provision a centos7 vm using virtualbox driver
 # with oc cluster up
 #
+PUBLIC_IP=192.168.99.50
 echo "=================="
 echo "Reset ssh key"
 echo "=================="
-ssh-keygen -R 192.168.99.50
+ssh-keygen -R $PUBLIC_IP
 echo "===================="
 echo "Create Virtualbox VM"
 echo "===================="
@@ -16,4 +17,4 @@ sleep 5m
 echo "============================================="
 echo "Execute shell script to perform oc cluster up"
 echo "============================================="
-ssh -o StrictHostKeyChecking=no root@192.168.99.50 'bash -s' < sandbox/cluster_up.sh
+ssh -o StrictHostKeyChecking=no root@$PUBLIC_IP 'bash -s' < sandbox/cluster_up.sh
