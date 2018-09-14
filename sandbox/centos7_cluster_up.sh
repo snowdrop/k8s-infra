c8b0a7c03c8a48cac12e1c08389191f24a8f6aa4
Charles Moulliard <cmoulliard@redhat.com>
2018-09-14 07:20:01 +0200

    Add wait and no host checking

--- sandbox/centos7_cluster_up.sh
+++ sandbox/centos7_cluster_up.sh
@@ -4,5 +4,16 @@
 # Command to provision a centos7 vm using virtualbox driver
 # with oc cluster up
 #
-../virtualbox/create-vm.sh -i ~/images -m 5000 -n dummy
-ssh root@192.168.99.50 'bash -s' < sandbox/cluster_up.sh
+echo "=================="
+echo "Reset ssh key"
+echo "=================="
+ssh-keygen -R 192.168.99.50
+echo "===================="
+echo "Create Virtualbox VM"
+echo "===================="
+./virtualbox/create-vm.sh -i ~/images -m 5000 -n dummy
+sleep 5m
+echo "============================================="
+echo "Execute shell script to perform oc cluster up"
+echo "============================================="
+ssh -o StrictHostKeyChecking=no root@192.168.99.50 'bash -s' < sandbox/cluster_up.sh
