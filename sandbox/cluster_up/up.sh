143aa82fc491f8483575547b8cfa2d91529b4f38
Charles Moulliard <cmoulliard@redhat.com>
2018-12-06 09:11:24 +0100

    Add version for --tag

--- sandbox/cluster_up/up.sh
+++ sandbox/cluster_up/up.sh
@@ -5,6 +5,7 @@ echo "Bootstrap oc"
 echo "==============================="
 PUBLIC_IP=$(ifconfig enp0s3 | sed -En 's/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 oc cluster up \
+  --tag=v3.11
   --base-dir="/var/lib/origin/openshift.local.clusterup" \
   --public-hostname=${PUBLIC_IP}
 
