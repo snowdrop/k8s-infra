25c249d62ab2436a387354bce14a5a0ef6501cef
Charles Moulliard <cmoulliard@redhat.com>
2019-04-19 18:48:55 +0200

    TODO. Investigate how launch bridge in the background

--- kubernetes/launch-console.sh
+++ kubernetes/launch-console.sh
@@ -4,17 +4,18 @@ mkdir -p ~/go/{bin,pkg,src}
 echo 'export GOPATH="$HOME/go"' >> ~/.bashrc
 echo 'export PATH="$PATH:${GOPATH//://bin:}/bin"' >> ~/.bashrc
 source ~/.bashrc
+sudo chown -R centos:centos /home/centos
 
 console_dir=/home/centos/go/src/github.com/openshift/console
 if [[ ! -e $console_dir ]]; then
     mkdir -p $console_dir
-    go get github.com/openshift/console && cd /home/centos/go/src/github.com/openshift/console
+    git clone https://github.com/openshift/console.git /home/centos/go/src/github.com/openshift/console && cd /home/centos/go/src/github.com/openshift/console
     ./build.sh
 else
-    echo "Directory exists"
+    echo "Directory exists. No need to clone again the Openshift Console"
 fi
 
 cd $console_dir
 export KUBECONFIG=/root/.kube/config
 source ./contrib/environment.sh
-./bin/bridge
+nohup ./bin/bridge
