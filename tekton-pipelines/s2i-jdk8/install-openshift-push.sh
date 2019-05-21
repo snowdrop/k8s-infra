66a6b9999e519cd9d817f35802d40625977ed99b
Charles Moulliard <cmoulliard@redhat.com>
2019-05-21 17:13:14 +0200

    Add log file for openshift

--- tekton-pipelines/s2i-jdk8/install-openshift-push.sh
+++ tekton-pipelines/s2i-jdk8/install-openshift-push.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-oc apply -f resources/sa.yml
-
+oc apply -f resources/sa-secret.yml
 oc adm policy add-scc-to-user privileged -z build-bot
 oc adm policy add-role-to-user edit -z build-bot
 
-oc apply -f tasks/clone-build.yml
-
-oc apply -f runtasks/build.yml
+oc apply -f tasks/clone-build-push.yml
+oc apply -f runtasks/build-push.yml
