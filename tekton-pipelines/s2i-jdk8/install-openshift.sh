0f0c4f70faf351baeacbe33e42a0c4a6cbc45928
Charles Moulliard <cmoulliard@redhat.com>
2019-05-21 17:01:24 +0200

    Comment line not needed

--- tekton-pipelines/s2i-jdk8/install-openshift.sh
+++ tekton-pipelines/s2i-jdk8/install-openshift.sh
@@ -6,9 +6,9 @@ oc adm policy add-scc-to-user privileged -z build-bot
 oc adm policy add-role-to-user edit -z build-bot
 
 oc apply -f tasks/clone-build.yml
-oc apply -f tasks/clone-build-push.yml
 
 oc apply -f runtasks/build.yml
 
+# oc apply -f tasks/clone-build-push.yml
 # oc apply -f resources/docker-secret.yml
 # oc apply -f runtasks/build-push.yml
