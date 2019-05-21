00de74031076b95f11a130ca8356b17e3fe60de8
Charles Moulliard <cmoulliard@redhat.com>
2019-05-21 12:41:16 +0200

    Define the resources to be installed individually

--- tekton-pipelines/s2i-jdk8/install.sh
+++ tekton-pipelines/s2i-jdk8/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-kubectl apply -f resources/
+kubectl apply -f resources/docker-secret.yml
+kubectl apply -f resources/sa.yml
 kubectl apply -f tasks/clone-build.yml
 kubectl apply -f runtasks/springboot-example.yml
