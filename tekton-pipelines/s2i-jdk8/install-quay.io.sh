6f445b764dd8d6a164f64ac5a26a41b76de279c3
Charles Moulliard <cmoulliard@redhat.com>
2019-05-22 14:57:15 +0200

    Update script to use quay.io registry to deploy the image

--- tekton-pipelines/s2i-jdk8/install-quay.io.sh
+++ tekton-pipelines/s2i-jdk8/install-quay.io.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+oc delete taskruns --all
+oc delete tasks --all
+oc delete serviceaccount --all
+oc delete deploymemt --all
+
 # s2i build as dockerfile -> buildah bud -> buildah push
 # Using external quay.io registry
-kubectl apply -f resources/docker-secret.yml
-kubectl apply -f resources/sa-secret.yml
+kubectl create namespace demo
 
-kubectl apply -f tasks/buildah-push.yml
-kubectl apply -f runtasks/buildah-push.yml
+kubectl apply -f resources/docker-secret.yml -n demo
+kubectl apply -f resources/sa-secret.yml -n demo
+
+kubectl apply -f tasks/buildah-push.yml -n demo
+kubectl apply -f runtasks/buildah-push-quay.io.yml -n demo
 
