1be2904c4362cbfe1450b2add17e38e864c7c446
Charles Moulliard <cmoulliard@redhat.com>
2019-08-26 16:39:19 +0200

    Use connection=local instead of the inventory file

--- hetzner/scripts/ansible-oc.sh
+++ hetzner/scripts/ansible-oc.sh
@@ -7,11 +7,12 @@ echo "Cloning Snowdrop Infra Playbook"
 git clone https://github.com/snowdrop/openshift-infra.git /tmp/infra 2>&1
 
 echo "Creating Ansible inventory file"
-echo -e "localhost ansible_connection=local ansible_user=root" > /tmp/infra/ansible/inventory/hetzner_vm
+# echo -e "localhost ansible_connection=local ansible_user=root" > /tmp/infra/ansible/inventory/hetzner_vm
 
 echo "Starting playbook"
 cd /tmp/infra/ansible
-ansible-playbook -i ./inventory/hetzner_vm playbook/cluster.yml \
+#ansible-playbook -i ./inventory/hetzner_vm playbook/cluster.yml \
+ansible-playbook --connection=local --inventory 127.0.0.1, playbook/cluster.yml \
     -e openshift_release_tag_name=v3.11.0 \
     --tags "up" \
     2>&1
