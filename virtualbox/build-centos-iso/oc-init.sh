5e716a15d6c02d8bd019249fd6e96d13855ce55a
Charles Moulliard <cmoulliard@redhat.com>
2019-08-26 10:25:48 +0200

    Use a bash script to install oc and configure docker

---
+++ virtualbox/build-centos-iso/oc-init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set +e
+
+version=3.11
+echo -e '{ \n   "insecure-registries" : [ "172.30.0.0/16" ],\n   "hosts" : [ "unix://", "tcp://0.0.0.0:2376" ]\n}' > /etc/docker/daemon.json
+rm -rf /etc/docker/certs.d/registry.access.redhat.com
+systemctl enable docker
+systemctl restart docker
+
+wget https://github.com/openshift/origin/releases/download/v${version}.0/openshift-origin-client-tools-v${version}.0-0cbc58b-linux-64bit.tar.gz
+tar -vxf openshift-origin-client-tools-v${version}.0-0cbc58b-linux-64bit.tar.gz
+sudo cp openshift-origin-client-tools-v${version}.0-0cbc58b-linux-64bit/oc /usr/local/bin
+
+echo "Launching oc startup using version 3.11"
+oc cluster up \
+  --tag=v3.11 \
+  --base-dir="/var/lib/origin/openshift.local.clusterup" \
+  --skip-registry-check=true \
+  --enable=[-sample-templates] \
+  --v=5 \
+  2>&1
+
+exit 0
