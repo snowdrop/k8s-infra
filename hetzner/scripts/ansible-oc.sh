e1d51c4cf9f54200ff20ff53311502af338fbe39
Charles Moulliard <cmoulliard@redhat.com>
2019-08-26 18:51:07 +0200

    Add commands to pull the docker images

--- hetzner/scripts/ansible-oc.sh
+++ hetzner/scripts/ansible-oc.sh
@@ -9,10 +9,23 @@ git clone https://github.com/snowdrop/openshift-infra.git /tmp/infra 2>&1
 echo "Creating Ansible inventory file"
 echo -e "[masters]\nlocalhost ansible_connection=local ansible_user=root" > /tmp/infra/ansible/inventory/hetzner_vm
 
+echo "Pulling docker images"
+docker pull quay.io/openshift/origin-node:v${version}
+docker pull quay.io/openshift/origin-control-plane:v${version}
+docker pull quay.io/openshift/origin-haproxy-router:v${version}
+docker pull quay.io/openshift/origin-hyperkube:v${version}
+docker pull quay.io/openshift/origin-deployer:v${version}
+docker pull quay.io/openshift/origin-pod:v${version}
+docker pull quay.io/openshift/origin-hypershift:v${version}
+docker pull quay.io/openshift/origin-cli:v${version}
+docker pull quay.io/openshift/origin-docker-registry:v${version}
+docker pull quay.io/openshift/origin-web-console:v${version}
+docker pull quay.io/openshift/origin-service-serving-cert-signer:v${version}
+
 echo "Starting playbook"
 cd /tmp/infra/ansible
 ansible-playbook -i ./inventory/hetzner_vm playbook/cluster.yml \
-    -e openshift_release_tag_name=v3.11.0 \
+    -e openshift_release_tag_name="v${version}.0" \
     -e public_ip_address="${hostIP}" \
     --tags "up" \
     2>&1
