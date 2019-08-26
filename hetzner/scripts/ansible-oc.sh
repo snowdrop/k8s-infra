afb7fc50549b3ef85b6e867408ab9a9e60a754db
Charles Moulliard <cmoulliard@redhat.com>
2019-08-26 16:27:54 +0200

    Fix wrong inventory file location

--- hetzner/scripts/ansible-oc.sh
+++ hetzner/scripts/ansible-oc.sh
@@ -7,11 +7,11 @@ echo "Cloning Snowdrop Infra Playbook"
 git clone https://github.com/snowdrop/openshift-infra.git /tmp/infra 2>&1
 
 echo "Creating Ansible inventory file"
-echo -e "[masters]\n ${hostIP} public_ip_address==${hostIP} ansible_user=root" > /tmp/infra/ansible/inventory/${hostIP}
+echo -e "[masters]\n ${hostIP} public_ip_address==${hostIP} ansible_user=root" > /tmp/infra/ansible/inventory/hetzner_vm
 
 echo "Starting playbook"
 cd /tmp/infra/ansible
-ansible-playbook -i inventory playbook/cluster.yml \
+ansible-playbook -i ./inventory/hetzner_vm playbook/cluster.yml \
     -e openshift_release_tag_name=v3.11.0 \
     --tags "up" \
     2>&1
