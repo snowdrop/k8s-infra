e449193ee852da6ab135e425b9f00dc1a38eee67
Charles Moulliard <cmoulliard@redhat.com>
2019-08-27 07:41:28 +0200

    Download oc release 3.11.0

--- hetzner/scripts/ansible-oc.sh
+++ hetzner/scripts/ansible-oc.sh
@@ -5,7 +5,7 @@ hostIP=$(hostname -I | awk '{print $1}')
 echo "Starting playbook"
 cd /tmp/infra/ansible
 ansible-playbook -i ./inventory/hetzner_vm playbook/cluster.yml \
-    -e openshift_release_tag_name="v3.11" \
+    -e openshift_release_tag_name="v3.11.0" \
     -e public_ip_address="${hostIP}" \
     --tags "up" \
     2>&1
