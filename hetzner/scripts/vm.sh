a5da2368caa2a0b5526fac201689b64c784db9fc
Charles Moulliard <cmoulliard@redhat.com>
2019-12-19 13:05:42 +0100

    chore: Add vm.sh script to only create a hetzner cloud vm

---
+++ hetzner/scripts/vm.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Script to be executed under Hetzner folder path in order
+# to create a new Hetzner VM using hcloud tool
+#
+
+# ./scripts/vm.sh VM_NAME VM_TYPE VM_IMAGE SALT_TEXT USER_PWD
+# e.g
+# ./scripts/vm.sh cloud1 on cx31 centos-7 <SALT_TEXT> <USER_GENERATED_PWD>
+
+VM_NAME=${1:-cloud1}
+VM_TYPE=${2:-cx31}
+VM_IMAGE=${3:-centos-7}
+SALT_TEXT=$4
+USER_PASSWORD=$5
+
+BASH_SCRIPTS_DIR=$(dirname $0)
+
+# Delete and create the Hetzner Cloud server
+. create-hcloud-server.sh
+
+echo "#######################################"
+echo "Execute the following command within a terminal to ssh to the vm"
+echo "alias ssh-${VM_NAME}=\"ssh -i ~/.ssh/id_hetzner_snowdrop root@${IP_HETZNER}\""
