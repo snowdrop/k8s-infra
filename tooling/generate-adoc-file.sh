48024b89246a714864b44a5253568707a9738ace
Charles Moulliard <cmoulliard@redhat.com>
2020-05-20 17:42:10 +0200

    fix: Add missing [] and upper level to create the correct path to access the README.adoc files

--- tooling/generate-adoc-file.sh
+++ tooling/generate-adoc-file.sh
@@ -5,7 +5,7 @@ paths=""
 
 for FILE in `find ${path} -name **.adoc`
 do
-  paths+="include::$FILE\n"
+  paths+="include::../$FILE[]\n"
 done
 
 awk -v paths=$paths '{gsub(/INCLUDES/,paths)}1' ./asciidoctor/all.adoc.tmp > ./asciidoctor/all.adoc
