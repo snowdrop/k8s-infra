576421060cd9d053c11013965a743620f448abd6
Charles Moulliard <cmoulliard@redhat.com>
2020-05-20 18:14:51 +0200

    chore: Rename the script and add the maven step to generate the HTML file

--- tooling/generate-ansible-roles-doc.sh
+++ tooling/generate-ansible-roles-doc.sh
@@ -8,4 +8,11 @@ do
   paths+="include::../$FILE[]\n\n"
 done
 
+echo "### Generate the all.adoc file containing the include:: directive for each role"
 awk -v paths=$paths '{gsub(/INCLUDES/,paths)}1' ./asciidoctor/all.adoc.tmp > ./asciidoctor/all.adoc
+
+echo "### Generate the HTML asciidoctor file containing the table of the roles and each role"
+mvn clean package -DskipTests=true
+
+
+
