398d57121ab657c5d38db0aa2adf190d2888f84f
Charles Moulliard <cmoulliard@redhat.com>
2020-05-20 17:00:11 +0200

    chore: Fix issue with var empty after loop

--- tooling/generate-adoc-file.sh
+++ tooling/generate-adoc-file.sh
@@ -1,13 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 path=../ansible/roles
 paths=""
 
-find ${path} -name **.adoc -print0 |
-    while IFS= read -r -d '' line; do
-        echo "$paths include::$line[]"
-    done
+for FILE in `find ${path} -name **.adoc`
+do
+  paths+="include::$FILE[]"$'\n'
+done
 
 sed "s|INCLUDES|${paths}|g" ./asciidoctor/all.adoc.tmp > ./asciidoctor/all.adoc
-
-echo ${paths}
