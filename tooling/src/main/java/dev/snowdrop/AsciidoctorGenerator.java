4cabc79f88018e266e859a7f2ce9ecbf6b0df72f
Antonio Costa <a.costa@trikorasolutions.com>
2021-03-15 11:02:54 +0100

    fix: changes to make the doc generator work again
    Updated the bash script so it doesn't use the PATH variable, create a build bash script, upgraded the asciidoctorj and snakeyaml versions

--- tooling/src/main/java/dev/snowdrop/AsciidoctorGenerator.java
+++ tooling/src/main/java/dev/snowdrop/AsciidoctorGenerator.java
@@ -1,52 +1,38 @@
 package dev.snowdrop;
 
-import dev.snowdrop.extension.CreateTableTreeProcessor;
-import dev.snowdrop.type.Config;
-import org.asciidoctor.Asciidoctor;
-import org.asciidoctor.OptionsBuilder;
-import org.asciidoctor.SafeMode;
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.Constructor;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
 
-import static dev.snowdrop.Helper.cfg;
+import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.Attributes;
+import org.asciidoctor.AttributesBuilder;
+import org.asciidoctor.OptionsBuilder;
+import org.asciidoctor.Placement;
+import org.asciidoctor.SafeMode;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+
+import dev.snowdrop.type.Config;
 
 public class AsciidoctorGenerator {
 
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(args));
         // Parse the yaml config
         Yaml yaml = new Yaml(new Constructor(Config.class));
+        Asciidoctor asciidoctor = Asciidoctor.Factory.create();
         try {
-            cfg = yaml.load(new FileReader(args[0]));
-
-            String adocFile = cfg.getAsciidoctorFile();
-            try (Asciidoctor asciidoctor = Asciidoctor.Factory.create()) {
-
-                // Include the Table extension
-                asciidoctor.javaExtensionRegistry()
-                        .treeprocessor(CreateTableTreeProcessor.class);
-
-                // Generate HTML
-                asciidoctor.convertFile(
-                        new File(adocFile),
-                        OptionsBuilder.options()
-                                .toFile(new File(cfg.getDestinationFile()))
-                                .safe(SafeMode.UNSAFE));
-
-                // Generate PDF
-            /*
-            asciidoctor.convertFile(
-            new File(adocFile),
-            OptionsBuilder.options()
-                    .backend("pdf")
-                    .toFile(true)
-                    .safe(SafeMode.UNSAFE));
-            */
-            }
+            Config cfg = yaml.load(new FileReader(args[0]));
+            String adocFiles = cfg.getDestinationFile();
+            File fileAdoc = new File(cfg.getAsciidoctorFile());
+            Attributes attributes = AttributesBuilder.attributes().backend("html5").icons("font")
+                    .tableOfContents(Placement.LEFT).tableOfContents(true).get();
+            asciidoctor.convertFile(fileAdoc, OptionsBuilder.options().attributes(attributes).backend("html5")
+                    .toFile(new File(cfg.getDestinationFile())).safe(SafeMode.UNSAFE));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
