69146352cc875da6148ebc9a7c6b06919b394439
Charles Moulliard <ch007m@gmail.com>
2021-09-30 10:20:30 +0200

    Fix the issue to access the docker container registry from a pod. Refactor the project to propose a beautiful banner. Test it again using kpack. Fix #219

--- kind/kind-tls-secured-reg.sh
+++ kind/kind-tls-secured-reg.sh
@@ -2,6 +2,24 @@
 
 set -o errexit
 
+#
+# Script creating a Kubernetes cluster using kind tool and :
+# - Deploying a local secured (using htpasswd) docker registry
+# - Generating a selfsigned certificate (using openssl) to expose the registry as a HTTP/HTTPS endpoint
+# - Setting a docker network between the 2 containers: kind and registry and alias "registry.local"
+# - Allowing to access the repository using as address "registry.local:5000" within a pod, from laptop or when a pod is created
+# - Exposing 2 additional NodePort: 30000 and 31000
+# - Deploying an ingress controller
+# - Copying the generated certificate here: $HOME/local-registry.crt
+#
+# Remark: Please add to your /etc/hosts file --> "127.0.0.1 registry.local kind-registry
+#
+# Creation: September 30th - 2021
+#
+# Add hereafter changes done post creation date as backlog
+#
+# MMMM dd:
+
 reg_name='kind-registry'
 reg_server='localhost'
 reg_port='5000'
@@ -10,15 +28,93 @@ reg_image_version='2.6.2'
 current_dir=$(pwd)
 temp_cert_dir=$(mktemp -d 2>/dev/null || mktemp -d -t 'temp_cert_dir')
 
-. ./functions/openssl.sh
+echo ""
+echo "Welcome to our"
+echo "                                                                   "
+echo "   _____                                  _                        "
+echo "  / ____|                                | |                       "
+echo " | (___    _ __     ___   __      __   __| |  _ __    ___    _ __  "
+echo "  \___ \  | '_ \   / _ \  \ \ /\ / /  / _  | |  __|  / _ \  | \ _ \ "
+echo "  ____) | | | | | | (_) |  \ V  V /  | (_| | | |    | (_) | | |_) |"
+echo " |_____/  |_| |_|  \___/    \_/\_/    \__,_| |_|     \___/  |  __/ "
+echo "                                                            | |    "
+echo "                                                            |_|    "
+echo " Kind installation script"
+echo ""
+echo "- Deploying a local secured (using htpasswd) docker registry"
+echo "- Generating a selfsigned certificate (using openssl) to expose the registry as a HTTP/HTTPS endpoint"
+echo "- Setting a docker network between the containers: kind and registry and alias \"registry.local\""
+echo "- Allowing to access the repository using as address \"registry.local:5000\" within a pod, from laptop or when a pod is created"
+echo "- Exposing 2 additional NodePort: 30000 and 31000"
+echo "- Deploying an ingress controller"
+echo "- Copying the generated certificate here: $HOME/local-registry.crt"
+echo ""
 
-echo "==== Deleting kind cluster ..."
-kind delete cluster
-echo "==== Deleting kind docker network ..."
-docker network rm kind || true
+read -p "Do you want to delete the kind cluster (y|n) - Default: y ? " cluster_delete
+cluster_delete=${cluster_delete:-y}
+read -p "Which kubernetes version should we install (1.14 .. 1.21 (= default) .. 1.22) - Default: default ? " version
+k8s_minor_version=${version:-default}
+read -p "What logging verbosity do you want to use with kind (0..9) - A verbosity setting of 0 logs only critical events - Default: 0 ? " logging_verbosity
+logging_verbosity=${logging_verbosity:-0}
 
+kindCmd="kind -v ${logging_verbosity} create cluster"
+
+create_openssl_cfg() {
+CFG=$(cat <<'EOF'
+[req]
+distinguished_name = subject
+x509_extensions    = x509_ext
+prompt             = no
+
+[subject]
+C  = BE
+ST = Namur
+L  = Florennes
+O  = Red Hat
+OU = Snowdrop
+CN = localhost
+
+[x509_ext]
+basicConstraints        = critical, CA:TRUE
+subjectKeyIdentifier    = hash
+authorityKeyIdentifier  = keyid:always, issuer:always
+keyUsage                = critical, cRLSign, digitalSignature, keyCertSign
+nsComment               = "OpenSSL Generated Certificate"
+subjectAltName          = @alt_names
+
+[alt_names]
+DNS.1 = kind-registry
+DNS.2 = localhost
+DNS.3 = registry.local
+EOF
+)
+echo "$CFG"
+}
+
+if [ "$cluster_delete" == "y" ]; then
+  echo "==== Deleting kind cluster ..."
+  kind delete cluster
+
+  echo "==== Stopping the container registry"
+  docker stop ${reg_name} || true && docker rm ${reg_name} || true
+
+  echo "==== Deleting the "kind" docker network ..."
+  docker network rm kind || true
+fi
+
+echo "=== Get the tag version of the image to be installed for the kubernetes version: ${k8s_minor_version}  ..."
+if [ "$k8s_minor_version" != "default" ]; then
+  patch_version=$(wget -q https://registry.hub.docker.com/v1/repositories/kindest/node/tags -O - | \
+  jq -r '.[].name' | grep -E "^v${k8s_minor_version}.[0-9]+$" | \
+  cut -d. -f3 | sort -rn | head -1)
+  k8s_version="v${k8s_minor_version}.${patch_version}"
+  kindCmd+=" --image kindest/node:${k8s_version}"
+else
+  k8s_version=$k8s_minor_version
+fi
+
+# Generate the Self signed certificate using openssl
 pushd $temp_cert_dir
-echo "==== Generate a self-signed certificate and user/pwd to secure the local registry"
 mkdir -p certs/${reg_server}
 
 echo "==== Generate the openssl config"
@@ -34,25 +130,20 @@ openssl req -x509 \
   -config req.cnf \
   -sha256
 
+# Kind configuration
 kindCfg=$(cat <<EOF
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 containerdConfigPatches:
 - |-
-  #[plugins."io.containerd.grpc.v1.cri".registry]
-  #  config_path = "/etc/containerd/certs.d"
-  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
-    # Issue 219 due to this definition --> endpoint = ["https://localhost:${reg_port}"]
-    endpoint = ["https://${reg_name}:${reg_port}"]
-  [plugins."io.containerd.grpc.v1.cri".registry.configs."localhost:${reg_port}".tls]
-    #insecure_skip_verify = true
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."registry.local:${reg_port}"]
+    endpoint = ["https://registry.local:${reg_port}"]
+  [plugins."io.containerd.grpc.v1.cri".registry.configs."registry.local:${reg_port}".tls]
     cert_file = "/etc/docker/certs.d/${reg_server}/client.crt"
     key_file  = "/etc/docker/certs.d/${reg_server}/client.key"
 nodes:
 - role: control-plane
   extraMounts:
-    #- containerPath: /etc/containerd/certs.d/kind-registry
-    #  hostPath: $(pwd)/certs/kind-registry
     - containerPath: /etc/docker/certs.d/${reg_server}
       hostPath: $(pwd)/certs/${reg_server}
   extraPortMappings:
@@ -62,14 +153,21 @@ nodes:
   - containerPort: 443
     hostPort: 443
     protocol: TCP
+  - containerPort: 30000
+    hostPort: 30000
+    protocol: tcp
+  - containerPort: 31000
+    hostPort: 31000
+    protocol: tcp
 EOF
 )
 
 #echo "==== Config cluster"
 #echo $kindCfg
 
-echo "==== Creating a Kind cluster"
-echo "${kindCfg}" | kind create cluster --config=-
+# The kind released version available are: https://github.com/kubernetes-sigs/kind/releases
+echo "Creating a Kind cluster using as version: ${k8s_version} and logging verbosity: ${logging_verbosity}"
+echo "${kindCfg}" | ${kindCmd} --config=-
 
 # Document the local registry
 # https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
@@ -89,10 +187,7 @@ echo "==== Create the htpasswd file where user: admin and password: snowdrop"
 mkdir -p auth
 docker run --entrypoint htpasswd registry:2.7.0 -Bbn admin snowdrop > auth/htpasswd
 
-echo "==== Stopping the container registry"
-docker stop ${reg_name} || true && docker rm ${reg_name} || true
-
-echo "==== Creating a new container registry"
+echo "==== Creating a docker registry"
 docker run -d \
   -p 5000:5000 \
   --restart=always \
@@ -108,7 +203,7 @@ docker run -d \
 
 # connect the container registry to the cluster network
 # (the network may already be connected)
-docker network connect kind "${reg_name}"
+docker network connect kind "${reg_name}" --alias registry.local || true
 
 # Upload the self-signed certificate to the kind container
 name="${name:-"kind"}"
@@ -139,6 +234,6 @@ cp ${certfile} $HOME/local-registry.crt
 popd
 
 # Deploy the nginx Ingress controller on k8s >= 1.19
-# echo "==== Deploy the nginx Ingress controller"
-# VERSION=$(curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/stable.txt)
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/$VERSION/deploy/static/provider/kind/deploy.yaml
+echo "==== Deploy the nginx Ingress controller"
+VERSION=$(curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/stable.txt)
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/${VERSION}/deploy/static/provider/kind/deploy.yaml
