c759d9219a07cae06a1d23ac50cdb21eee975639
Antonio Costa <antcosta@redhat.com>
2022-06-20 12:23:55 +0200

    feat: missing user defaults to ansible_user and alternate ssh keys

--- tools/passstore-vm-ssh.sh
+++ tools/passstore-vm-ssh.sh
@@ -24,13 +24,33 @@ fi
 
 if [ ! -f ${SSH_KEY} ]; 
 then
-  echo "pass show ${VM_PROVIDER}/${VM_NAME}/id_rsa)" > ${SSH_KEY}
-  chmod 600 ${SSH_KEY}
+  SSH_KEY=~/.ssh/id_rsa_snowdrop_${VM_PROVIDER}
+  if [ ! -f ${SSH_KEY} ]; 
+  then
+    SSH_KEY=~/.ssh/id_rsa_snowdrop
+    if [ ! -f ${SSH_KEY} ]; 
+    then
+      echo "pass show ${VM_PROVIDER}/${VM_NAME}/id_rsa)" > ${SSH_KEY}
+      chmod 600 ${SSH_KEY}
+    fi
+  fi
 fi
 
 IP=$(pass show ${VM_PROVIDER}/${VM_NAME}/ansible_ssh_host | awk 'NR==1{print $1}')
 PORT=$(pass show ${VM_PROVIDER}/${VM_NAME}/ansible_ssh_port | awk 'NR==1{print $1}')
+
+if [ "$PORT" = "" ];
+then
+    PORT=22
+fi
+
 USER=$(pass show ${VM_PROVIDER}/${VM_NAME}/os_user | awk 'NR==1{print $1}')
+
+if [ "$USER" = "" ];
+then
+    USER=$(pass show ${VM_PROVIDER}/${VM_NAME}/ansible_user | awk 'NR==1{print $1}')
+fi
+
 
 echo "### SSH COMMAND: ssh -i ${SSH_KEY} ${USER}@${IP} -p ${PORT} ${@:4}"
 ssh -i ${SSH_KEY} ${USER}@${IP} -p ${PORT} "${@:4}"
