cd7d782d835c84d7278ea442097f08e9681f9531
Charles Moulliard <cmoulliard@redhat.com>
2022-09-15 09:36:42 +0200

    Declaring a jq var to pass the version of k8s to query

--- kind/kind-reg-ingress.sh
+++ kind/kind-reg-ingress.sh
@@ -121,7 +121,7 @@ if [ ${version} == "latest" ]; then
   kindCmd+=""
 else
   kind_image_sha=$(wget -q https://raw.githubusercontent.com/snowdrop/k8s-infra/main/kind/images.json -O - | \
-  jq -r '.[] | select(.k8s == "1.18").sha')
+  jq -r --arg VERSION "$version" '.[] | select(.k8s == $VERSION).sha')
   kindCmd+=" --image ${kind_image_sha}"
 fi
 
